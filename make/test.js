425c2fe75f96782c45c94562b260a7b18362469a
Lance Pollard <lp@elk.fm>
2024-05-27 04:58:15 -0700

    save

--- make/test.js
+++
@@ -1,9 +0,0 @@
-import kink from './example.js';
-try {
-    throw kink('syntax_error', { foo: 'bar' });
-}
-catch (e) {
-    console.log(e);
-}
-console.log('done');
-//# sourceMappingURL=test.js.map
